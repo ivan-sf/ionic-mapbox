0de1e94397e487745078d392e8b0a8b7d0740813
Ivan Santander <ivansantander2020@gmail.com>
2022-06-26 07:49:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'homelander',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
